e7270546a08ca6a54dd5defab39661f504c40c58
Filipe Pimenta de Souza <filipe.souza@4secglobal.com>
2024-01-30 10:31:14 -0300

    commitHbase

--- build-image.sh
+++ build-image.sh
@@ -4,7 +4,7 @@ image=$1
 tag='latest'
 
 
-if [ $1 = 0 ]
+if [ $1 -eq 0 ]
 then
 	echo "Please use image name as the first argument!"
 	exit 1
@@ -13,8 +13,8 @@ fi
 # founction for delete images
 function docker_rmi()
 {
-	echo -e "\n\nsudo docker rmi krejcmat/$1:$tag"
-	sudo docker rmi krejcmat/$1:$tag
+	echo -e "\n\ndocker rmi krejcmat/$1:$tag"
+	docker rmi krejcmat/$1:$tag
 }
 
 
@@ -22,15 +22,15 @@ function docker_rmi()
 function docker_build()
 {
 	cd $1
-	echo -e "\n\nsudo docker build -t krejcmat/$1:$tag ."
-	/usr/bin/time -f "real  %e" sudo docker build -t krejcmat/$1:$tag .
+	echo -e "\n\ndocker build -t krejcmat/$1:$tag ."
+	docker build -t krejcmat/$1:$tag .
 	cd ..
 }
 
 echo -e "\ndocker rm -f slave1.krejcmat.com slave2.krejcmat.com master.krejcmat.com"
-sudo docker rm -f slave1.krejcmat.com slave2.krejcmat.com master.krejcmat.com
+docker rm -f slave1.krejcmat.com slave2.krejcmat.com master.krejcmat.com
 
-sudo docker images >images.txt
+docker images >images.txt
 
 #all image is based on dnsmasq. master and slaves are based on base image.
 if [ $image == "hadoop-hbase-base" ]
@@ -60,4 +60,4 @@ cat images.txt
 rm images.txt
 
 echo -e "\nimages after build"
-sudo docker images
+docker images
