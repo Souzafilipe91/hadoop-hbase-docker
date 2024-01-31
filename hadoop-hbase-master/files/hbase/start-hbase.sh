2d13780ad2d7624962c9ea866c162a035972f8cc
Filipe Pimenta de Souza <filipe.souza@4secglobal.com>
2024-01-31 09:08:27 -0300

    commithbase14

--- hadoop-hbase-master/files/hbase/start-hbase.sh
+++ hadoop-hbase-master/files/hbase/start-hbase.sh
@@ -5,7 +5,7 @@ hadoop_slave="slave1"    # Substitua pelo nome real do seu contêiner Hadoop Sla
 hbase_home="D:/hadoop-hbase-docker/hadoop-hbase-master/files/hbase"
 
 echo -e "starting hbase on master \n"
-docker exec -it "$hadoop_master" "${hbase_home}/start-hbase.sh"
+winpty docker exec -it "$hadoop_master" "${hbase_home}/start-hbase.sh"
 
 echo -e "starting hbase on slaves \n"
 docker exec -it "$hadoop_slave" "${hbase_home}/start-hbase.sh"
@@ -13,4 +13,4 @@ docker exec -it "$hadoop_slave" "${hbase_home}/start-hbase.sh"
 # Restante do seu script...
 sleep 5
 echo -e "starting hbase shell  \n"
-docker exec -it "$hadoop_master" /bin/hbase shell
+winpty docker exec -it "$hadoop_master" /bin/hbase shell
