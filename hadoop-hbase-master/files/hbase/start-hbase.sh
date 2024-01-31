219994e6f36b03e9e1f55aa416988db325f9474b
Filipe Pimenta de Souza <filipe.souza@4secglobal.com>
2024-01-31 09:10:28 -0300

    commithbase15

--- hadoop-hbase-master/files/hbase/start-hbase.sh
+++ hadoop-hbase-master/files/hbase/start-hbase.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-hadoop_master="serene_greider"  # Substitua pelo nome real do seu contêiner Hadoop Master
+hadoop_master="serene-greider"  # Substitua pelo nome real do seu contêiner Hadoop Master
 hadoop_slave="slave1"    # Substitua pelo nome real do seu contêiner Hadoop Slave
-hbase_home="D:/hadoop-hbase-docker/hadoop-hbase-master/files/hbase"
+hbase_home="/D/hadoop-hbase-docker/hadoop-hbase-master/files/hbase"  # Atualize para o caminho correto do diretório hbase
 
 echo -e "starting hbase on master \n"
 winpty docker exec -it "$hadoop_master" "${hbase_home}/start-hbase.sh"
@@ -13,4 +13,4 @@ docker exec -it "$hadoop_slave" "${hbase_home}/start-hbase.sh"
 # Restante do seu script...
 sleep 5
 echo -e "starting hbase shell  \n"
-winpty docker exec -it "$hadoop_master" /bin/hbase shell
+winpty docker exec -it "$hadoop_master" "${hbase_home}/bin/hbase" shell
