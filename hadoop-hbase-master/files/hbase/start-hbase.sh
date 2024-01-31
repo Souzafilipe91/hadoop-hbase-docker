0fff7cb3cb6bd56693aef8c0eef6019d75477bf9
Filipe Pimenta de Souza <filipe.souza@4secglobal.com>
2024-01-31 09:03:34 -0300

    commithbase13

--- hadoop-hbase-master/files/hbase/start-hbase.sh
+++ hadoop-hbase-master/files/hbase/start-hbase.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
-#hadoop_home=/usr/local/hbase
 
 hadoop_master="serene-greider"  # Substitua pelo nome real do seu contêiner Hadoop Master
 hadoop_slave="slave1"    # Substitua pelo nome real do seu contêiner Hadoop Slave
 hbase_home="D:/hadoop-hbase-docker/hadoop-hbase-master/files/hbase"
 
-cd $hadoop_home
-echo -e "starting hbase \n"
-${hbase_home}/start-hbase.sh
+echo -e "starting hbase on master \n"
+docker exec -it "$hadoop_master" "${hbase_home}/start-hbase.sh"
 
-#echo -e "starting local master beckup \n"
-#./bin/local-master-backup.sh start 1
-#The number at the end of the command signifies an offset that is added to the default
-#ports of 60000 for RPC and 60010 for the web-based UI. In this example, a new master
-#process would be started that reads the same configuration files as usual, but would
-#listen on ports 60001 and 60011 , respectively.
+echo -e "starting hbase on slaves \n"
+docker exec -it "$hadoop_slave" "${hbase_home}/start-hbase.sh"
 
-#echo -e "starting local regionserver \n"
-#./bin/local-regionservers.sh start 3
+# Restante do seu script...
 sleep 5
 echo -e "starting hbase shell  \n"
-./bin/hbase shell
+docker exec -it "$hadoop_master" /bin/hbase shell
