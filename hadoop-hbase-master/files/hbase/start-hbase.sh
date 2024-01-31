f67f33e16e62b8058f6c4fe462327a5a150f7d1b
Filipe Pimenta de Souza <filipe.souza@4secglobal.com>
2024-01-31 14:32:31 -0300

    commithbase34

--- hadoop-hbase-master/files/hbase/start-hbase.sh
+++ hadoop-hbase-master/files/hbase/start-hbase.sh
@@ -10,10 +10,10 @@ winpty docker cp "$script_path_on_host" "$hadoop_master:$script_path_in_containe
 winpty docker cp "$script_path_on_host" "$hadoop_slave:$script_path_in_container"
 
 echo -e "starting hbase on master \n"
-winpty docker exec -it "$hadoop_master" bash -c "cd /usr/local/hbase && $script_path_in_container"
+winpty docker exec -it "$hadoop_master" bash -c "chmod +x $script_path_in_container && cd /usr/local/hbase && $script_path_in_container"
 
 echo -e "starting hbase on slaves \n"
-winpty docker exec -it "$hadoop_slave" bash -c "cd /usr/local/hbase && $script_path_in_container"
+winpty docker exec -it "$hadoop_slave" bash -c "chmod +x $script_path_in_container && cd /usr/local/hbase && $script_path_in_container"
 
 sleep 5
 echo -e "starting hbase shell  \n"
