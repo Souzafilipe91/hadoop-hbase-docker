82d0fb925186ed79039e558012e9f0cb88a98417
Filipe Pimenta de Souza <filipe.souza@4secglobal.com>
2024-01-31 14:27:08 -0300

    commit33

--- hadoop-hbase-master/files/hbase/start-hbase.sh
+++ hadoop-hbase-master/files/hbase/start-hbase.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#hadoop_home=/usr/local/hbase
 
 hadoop_master="serene_greider"
-hadoop_slave="slave1"   
-hbase_home="/usr/local/hbase"
+hadoop_slave="slave1"
+script_path_on_host="D:/hadoop-hbase-docker/hadoop-hbase-master/files/hbase/start-hbase.sh"
+script_path_in_container="/usr/local/hbase/start-hbase.sh"
 
-start_hbase_script="/usr/local/hbase/hbase/start-hbase.sh"
+echo -e "Copying script to container \n"
+winpty docker cp "$script_path_on_host" "$hadoop_master:$script_path_in_container"
+winpty docker cp "$script_path_on_host" "$hadoop_slave:$script_path_in_container"
 
 echo -e "starting hbase on master \n"
-winpty docker exec -it "$hadoop_master" bash -c "cd \"$start_hbase_script\" && ./start-hbase.sh"
+winpty docker exec -it "$hadoop_master" bash -c "cd /usr/local/hbase && $script_path_in_container"
 
 echo -e "starting hbase on slaves \n"
-winpty docker exec -it "$hadoop_slave" bash -c "cd \"$start_hbase_script\" && ./start-hbase.sh"
+winpty docker exec -it "$hadoop_slave" bash -c "cd /usr/local/hbase && $script_path_in_container"
 
 sleep 5
 echo -e "starting hbase shell  \n"
-winpty docker exec -it "$hadoop_master" bash -c "cd \"$start_hbase_script\" && ./bin/hbase shell"
+winpty docker exec -it "$hadoop_master" bash -c "cd /usr/local/hbase && ./bin/hbase shell"
