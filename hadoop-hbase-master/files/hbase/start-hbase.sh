f5499bb01341f23af35435ebe18a8dfe063211e3
Filipe Pimenta de Souza <filipe.souza@4secglobal.com>
2024-01-31 14:12:39 -0300

    commithbase27

--- hadoop-hbase-master/files/hbase/start-hbase.sh
+++ hadoop-hbase-master/files/hbase/start-hbase.sh
@@ -6,11 +6,11 @@ hadoop_slave="slave1"
 hbase_home="/usr/local/hbase"
 
 echo -e "starting hbase on master \n"
-docker exec -it serene_greider bash -c "cd $hbase_home/hbase && ./start-hbase.sh"
+winpty docker exec -it serene_greider bash -c "cd $hbase_home/hbase && ./start-hbase.sh"
 
 echo -e "starting hbase on slaves \n"
-docker exec -it slave1 bash -c "cd $hbase_home/hbase && ./start-hbase.sh"
+winpty docker exec -it slave1 bash -c "cd $hbase_home/hbase && ./start-hbase.sh"
 
 sleep 5
 echo -e "starting hbase shell  \n"
-docker exec -it serene_greider bash -c "cd \"$hbase_home\" && ./bin/hbase shell"
+winpty exec -it serene_greider bash -c "cd \"$hbase_home\" && ./bin/hbase shell"
